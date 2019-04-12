b00ddb4276d11bc461f7d2c8a63d6b053a58b5be
hellozin <paul26375@gmail.com>
2019-04-13 00:08:26 +0900

    JSON Convertor 사용

--- src/main/java/me/hellozin/demospringmvc/SampleController.java
+++ src/main/java/me/hellozin/demospringmvc/SampleController.java
@@ -1,6 +1,7 @@
 package me.hellozin.demospringmvc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class SampleController {
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
+    }
+
+    @GetMapping("/jsonMessage")
+    public Person jsonMessage(@RequestBody Person person) {
+        return person;
     }
 
 }
--- src/test/java/me/hellozin/demospringmvc/SampleControllerTest.java
+++ src/test/java/me/hellozin/demospringmvc/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package me.hellozin.demospringmvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +27,9 @@ public class SampleControllerTest {
 
     @Autowired
     PersonRepository personRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void hello() throws Exception {
@@ -46,6 +51,22 @@ public class SampleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(Matchers.containsString("Hello Sample")))
                 .andExpect(header().exists(HttpHeaders.CACHE_CONTROL))
+        ;
+    }
+
+    @Test
+    public void jsonMessage() throws Exception {
+        Person person = new Person();
+        person.setId(100l);
+        person.setName("hellozin");
+
+        String jsonString = objectMapper.writeValueAsString(person);
+        this.mockMvc.perform(get("/jsonMessage")
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(print())
+                .andExpect(status().isOk())
         ;
     }
 }
