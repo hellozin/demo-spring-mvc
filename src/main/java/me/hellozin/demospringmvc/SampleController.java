79409048cd09a72de28a9a9484a68daa3a57ad2a
hellozin <paul26375@gmail.com>
2019-04-14 01:46:27 +0900

    다양한 Mapping 방법

--- src/main/java/me/hellozin/demospringmvc/SampleController.java
+++ src/main/java/me/hellozin/demospringmvc/SampleController.java
@@ -1,16 +1,37 @@
 package me.hellozin.demospringmvc;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping("/hello")
-    @ResponseBody
-    public String hello() {
+    @GetHelloMapping
+    public String customHello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/{name:[a-z]+}")
+    @ResponseBody
+    public String hello(@PathVariable String name) {
+        return "hello " + name;
+    }
+
+    @RequestMapping(value = "/json", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String jsonHandler() {
+        return "json";
+    }
+
+    @GetMapping(value = "/header", headers = HttpHeaders.AUTHORIZATION+"=key")
+    @ResponseBody
+    public String header() {
+        return "header";
     }
 
 }
--- src/test/java/me/hellozin/demospringmvc/SampleControllerTest.java
+++ src/test/java/me/hellozin/demospringmvc/SampleControllerTest.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,9 +23,31 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello/ahn"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello ahn"));
+
+        /* RFD Attack 방지 */
+        mockMvc.perform(get("/hell o/ahn.html"))
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(get("/json").contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andDo (print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void headerTest() throws Exception {
+        mockMvc.perform(get("/header").header(HttpHeaders.AUTHORIZATION, "key"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void customAnnotationTest() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
+                .andExpect(status().isOk());
     }
+
 }
