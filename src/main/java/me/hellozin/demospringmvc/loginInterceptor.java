71357551acce5a9df181cf442ff94f2c86022af5
hellozin <paul26375@gmail.com>
2019-04-21 19:46:49 +0900

    로그인 권한 인터셉터

--- src/main/java/me/hellozin/demospringmvc/loginInterceptor.java
+++ src/main/java/me/hellozin/demospringmvc/loginInterceptor.java
@@ -1,4 +1,21 @@
 package me.hellozin.demospringmvc;
 
-public class loginInterceptor {
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class loginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("visitor") == null) {
+            session.setAttribute("message", "Not");
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
 }
