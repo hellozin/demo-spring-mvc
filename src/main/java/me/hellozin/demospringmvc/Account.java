21f540f9d7af6a8245cc521cb1013d65985ba967
hellozin <paul26375@gmail.com>
2019-04-22 14:09:42 +0900

    2. Entitiy 타입 매핑

--- src/main/java/me/hellozin/demospringmvc/Account.java
+++ src/main/java/me/hellozin/demospringmvc/Account.java
@@ -3,10 +3,8 @@ package me.hellozin.demospringmvc;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
@@ -15,9 +13,15 @@ public class Account {
     @Id @GeneratedValue
     private Long id;
 
-//    @Column 생략
+    @Column(nullable = false)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+
+    @Transient
+    private String notColumnMapping;
 
 }
